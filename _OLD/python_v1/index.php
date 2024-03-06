59301b4c42c44e914bca34f9eed0f9ff48e5bf48
Roberto Nai <104762595+roberto-nai@users.noreply.github.com>
2024-03-06 15:28:37 +0100

    Delete _OLD directory

--- _OLD/python_v1/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$newURL = "./page-01.php";
-
-header('Location: '. $newURL);
-
-die();
-
-?>
--- _OLD/python_v2/box_2.php
+++
@@ -1,8 +0,0 @@
-<?php
-?>
-
-<img src="images-tutorial/variabili.png" alt="Variabili" class="img_box"/>
-<div class="box_content">
-    <h4>Variabili e conversioni</h4>
-    <a href="page-01.php"><button type="button" class="btn btn-secondary btn-lg" id="btnNext">Inizia</button></a>
-</div>
