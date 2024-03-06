f320603c37659af66fa1ea1fe749edc4e8c09066
Roberto Nai (UniTO) <roberto.nai@unito.it>
2024-03-06 15:31:10 +0100

    Update 2024-03-06

--- _OLD/python_v2/box_1.php
+++
@@ -1,8 +0,0 @@
-<?php
-?>
-
-<img src="images-tutorial/liste.png" alt="Liste" class="img_box"/>
-<div class="box_content">
-    <h4>Liste e Dizionari</h4>
-    <a href="page-01.php"><button type="button" class="btn btn-secondary btn-lg" id="btnNext">Inizia</button></a>
-</div>
